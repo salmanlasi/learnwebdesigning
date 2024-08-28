484ef60db2211f7fe01df2314cac4154250f9aae
unknown <salmanlasi>
2024-08-28 01:12:50 -0700

    Updates

---
+++ precache-manifest.8e6693980bd1befd948f5d17bcff0f3d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05c1ec835f427f2f42febe3c9641b9d9",
+    "url": "/learnwebdesigning/index.html"
+  },
+  {
+    "revision": "d71390bb47a52f196fed",
+    "url": "/learnwebdesigning/static/css/2.83d756e6.chunk.css"
+  },
+  {
+    "revision": "f4f9923d2e68e62d2685",
+    "url": "/learnwebdesigning/static/css/main.f8d8b519.chunk.css"
+  },
+  {
+    "revision": "d71390bb47a52f196fed",
+    "url": "/learnwebdesigning/static/js/2.089906c7.chunk.js"
+  },
+  {
+    "revision": "0eb3ca9d0061837f6c8f",
+    "url": "/learnwebdesigning/static/js/3.31c88699.chunk.js"
+  },
+  {
+    "revision": "f4f9923d2e68e62d2685",
+    "url": "/learnwebdesigning/static/js/main.b6f1b8a6.chunk.js"
+  },
+  {
+    "revision": "5b501be1510aeb813bd9",
+    "url": "/learnwebdesigning/static/js/runtime~main.24977332.js"
+  }
+]);
